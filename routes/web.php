c33b742d287735018f6b23dd30373b066098901d
ElenaRieux <lele.rieux@gmail.com>
2024-07-09 13:01:22 +0200

    index

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return 'hi';
 });
 
 Route::get('/reset-password/{token}', function (string $token) {
