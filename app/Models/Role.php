60895132714ab5b84b33d59dddff6e2ced44e1be
ElenaRieux <lele.rieux@gmail.com>
2024-07-11 12:31:14 +0200

    roles

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -17,9 +17,9 @@ class Role extends Model
 
     public function users()
     {
-        return $this->belongsToMany(User::class);
+        return $this->belongsToMany(User::class, 'user_roles', 'role_id', 'user_id');
     }
-
+    
     public function permissions()
     {
         return $this->belongsToMany(Permission::class, 'role_permissions', 'role_id', 'permission_id');
